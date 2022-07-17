cb47210c7a229ba20306719a465013c2125b037a
koridesu <37594761+koridesu@users.noreply.github.com>
2022-07-17 19:50:11 +0300

    Card positioning and styles improved

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Item } from '../interfaces/item-interface';
 import { ItemContext } from '../store/item-context';
 import classes from './Filter.module.css';
 
--- src/components/UI/ItemCard.tsx
+++ src/components/UI/ItemCard.tsx
@@ -9,11 +9,11 @@ interface Props {
 function ItemCard(props: Props) {
   return (
     <div className={classes['item-card']}>
-      <div className='img-container'>
-        {/* <img src={props.item.avatar} alt={props.item.avatar}></img> */}
+      <div className={classes['img-container']}>
+        <img src={props.item.avatar} alt={props.item.avatar}></img>
       </div>
       <p className='item-name'>{props.item.name}</p>
-      <p className='item-price'>{props.item.price}</p>
+      <p className='item-price'>${props.item.price}</p>
     </div>
   );
 }
